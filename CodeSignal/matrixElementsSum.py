4e0b39228fcb1092d30d8e30ea13b344957ae9ed
Yujin Byeon <63533661+ByeonYujin@users.noreply.github.com>
2020-08-28 23:59:11 +0900

    matrixElementsSum
    2차원 배열에서 특정 원소의 값이 0이면 그 행의 다음 원소들은 패스, 0이 아니면 값을 더한다.

---
+++ CodeSignal/matrixElementsSum.py
@@ -0,0 +1,12 @@
+def matrixElementsSum(matrix):
+    preRow = list(matrix[0])
+    result = sum(preRow)
+
+    for i in range(1, len(matrix)):
+        for j in range(len(matrix[0])):
+            if preRow[j] != 0:
+                result += matrix[i][j]
+            else:
+                continue
+            preRow[j] = matrix[i][j]
+    return result
