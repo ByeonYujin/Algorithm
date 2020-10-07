06b72ee70a19678117a1ec1aecf5c8135f4f3e24
Yujin Byeon <63533661+ByeonYujin@users.noreply.github.com>
2020-10-07 22:32:31 +0900

    itemCounts
    GROUP BY, ORDER BY statement for two attributes

---
+++ CodeSignal/itemCounts.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE itemCounts()
+BEGIN
+    SELECT item_name, item_type, COUNT(id) AS item_count FROM availableItems
+    GROUP BY item_name, item_type 
+    ORDER BY item_type, item_name;
+END
